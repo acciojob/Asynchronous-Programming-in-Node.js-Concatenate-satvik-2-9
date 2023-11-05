5d40748948e76d00b49ab501c3344fc6814d78a5
thelastenvoy <satvikbatra@gmail.com>
2023-11-05 18:49:41 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,41 @@
 const fs = require('fs');
 
 const filePaths = process.argv.slice(2);
-let concatenatedContent = '';
 
-filePaths.forEach((filePath) => {
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error(`Error reading file ${filePath}: ${err}`);
-      return;
-    }
-    concatenatedContent += data;
-    if (filePaths.indexOf(filePath) === filePaths.length - 1) {
-      fs.writeFile('output.txt', concatenatedContent, (err) => {
-        if (err) {
-          console.error(`Error writing file: ${err}`);
-          return;
-        }
-        console.log('Concatenated content written to output.txt');
-      });
-    }
+function readFilePromise(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(`Error reading file ${filePath}: ${err}`);
+      } else {
+        resolve(data);
+      }
+    });
   });
-});
+}
+
+function concatenateFiles(paths) {
+  return Promise.all(paths.map(readFilePromise))
+    .then(contents => {
+      const concatenatedContent = contents.join('');
+      return new Promise((resolve, reject) => {
+        /* since writeFile is an asynchronouse function, we need to wrap this in a Promise as well. */
+        fs.writeFile('output.txt', concatenatedContent, err => {
+          if (err) {
+            console.error(`Error writing file: ${err}`);
+            reject(err);
+          } else {
+            console.log('Concatenated content written to output.txt');
+            resolve();
+          }
+        });
+      });
+    });
+}
+
+// Check if this module is the main module, i.e., not required by another module.
+if (require.main === module) {
+  concatenateFiles(filePaths).catch(console.error);
+}
+
+module.exports = { readFilePromise, concatenateFiles };
